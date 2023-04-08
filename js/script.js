88cd6a7f317f5e4d8865cbf8ddaef9c7c3ab945d
Dundaki <vigjoaopaulo@gmail.com>
2023-04-07 21:14:40 -0300

    teste perfil

--- js/script.js
+++ js/script.js
@@ -15,5 +15,37 @@ const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
 
 
+var som = document.getElementById("som")
+function mouseOver(){
+ // som.play()
+ if(audio.pause){
+    som.play()
+  }else{
+    som.pause()
+  }
+  
+  
+  
+  
+  
+}
 
-    
+/* 
+var som = $("#som")[0]
+$(".globo")
+.mouseenter(function(){
+  som.pause()
+  som.currentTime = 0
+ 
+  som.play()
+  
+});*/
+
+/*
+var som = document.getElementById("som")
+
+$("globo").mouseover(function(){
+  som.pause()
+  som.currentTime = 0
+  som.play()
+})*/
