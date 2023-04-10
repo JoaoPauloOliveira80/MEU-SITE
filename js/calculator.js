a7ae6290c46e21d886ac0f4ad5cd1c102c2187b4
Dundaki <vigjoaopaulo@gmail.com>
2023-04-10 11:47:29 -0300

    calculadora alterada

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,29 @@
-const calculator = document.querySelector('.calculator')
+console.log('calculadora funcionando ')
 
 
-console.log(calculator)
+function insert(num)
+{
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
+}
